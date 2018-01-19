3b009fb731b3d93a919ac01b6fa593e02a7f6510
Danilo Guerrera <danilo.guerrera@unibas.ch>
2018-01-19 16:05:28 +0100

    added setup.cfg fixed setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """stempel setup file."""
 
 # Always prefer setuptools over distutils
@@ -83,7 +83,6 @@ setup(
         # that you indicate wheter you support Python2, Python 3 or both.
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
 
     # What doesd your project relate to?
@@ -92,7 +91,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
@@ -114,6 +113,11 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
+    package_data={
+        'stempel': ['headers/*.c', 'headers/*.h', 'headers/Makefile'],
+        'examples': ['machine-files/*.yaml', 'machine-files/*.yml'],
+        'tests': ['testfiles/*.yml'],
+    },
     include_package_data=True,
 
     # Although 'package_data' is the preferred approach, in some case you may
